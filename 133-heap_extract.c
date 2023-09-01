a31c2bf9cdee633de536fc7d8eca6c33547f5321
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-01 17:52:15 +0300

    Add tree node to extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: pointer to parent node
+ * @value: value of node
+ *
+ * Return: pointer to the newly created node or NULL if there is failure.
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = new_node->left = NULL;
+
+	return (new_node);
+}
+
+/**
  * find_last_node - finds last node on the last level in Max Binary Heap
  * @root: a double pointer to the root node of the Heap to insert the value
  *
